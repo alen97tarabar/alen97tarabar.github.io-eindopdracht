72b74d61cc5d2a843af5f9b4d059ee1c0cbe008c
alen97tarabar <69684780+alen97tarabar@users.noreply.github.com>
2021-05-04 20:57:46 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,16 @@
 <?php
 
-$dsn = 'mysql:dbname=cv_website;host=localhost';
-$user = 'root';
-$password = '';
+//Development Connection
+//$dsn = 'mysql:dbname=cv_website;host=localhost';
+//$user = 'root';
+//$password = '';
+
+//$db = new PDO($dsn, $user, $password);
+
+//remote connection
+$dsn = 'mysql:dbname=HINVquFDbs;host=remotemysql.com';
+$user = 'HINVquFDbs';
+$password = 'BTySh59Ugr';
 
 $db = new PDO($dsn, $user, $password);
 
