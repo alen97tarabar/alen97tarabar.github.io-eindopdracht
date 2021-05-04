fa857c1455ddeb93d4c55fffb2d2b49513451b20
alen97tarabar <69684780+alen97tarabar@users.noreply.github.com>
2021-05-04 21:09:45 +0200

    updated composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '72b74d61cc5d2a843af5f9b4d059ee1c0cbe008c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72b74d61cc5d2a843af5f9b4d059ee1c0cbe008c',
+    ),
+  ),
+);
