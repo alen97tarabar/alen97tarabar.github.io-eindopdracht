aa9d696ab871ff60d99e387dc12390eaad32e5ad
alen97tarabar <69684780+alen97tarabar@users.noreply.github.com>
2021-05-06 16:57:36 +0200

    changed form

--- review_form.php
+++ review_form.php
@@ -59,35 +59,46 @@ $formRatingWebsite = $row['rating_website'];
 <br>
 <br>
 <form id="formulier" action="review_form_stmt_insert.php" method="post">
-<p class="tableForm">
+<div class="form-row">
+<div class="col-md-2 mb-1">
     <label for="naam"> Naam: </label>
-    <input id="naam" type="text" name="naam">
-</p>
-<p class="tableForm">
+    <input id="naam" type="text" class="form-control is-valid" name="naam" required>
+
     <label for="email"> Email: </label>
-    <input id="email" type="text" name="email">
-</p>
-<p class="tableForm">
+    <input id="email" type="email" class="form-control is-valid" name="email" required>
+
     <label for="functie"> Functie: </label>
-    <input id="functie" type="text" name="functie">
-</p>
-<p class="tableForm">
+    <input id="functie" type="text" class="form-control is-valid" name="functie" required>
+
     <label for="datum"> Datum: </label>
-    <input id="datum" type="text" name="datum" value="yyyy/mm/dd">
-</p>
-<p class="tableForm">
+    <input id="datum" type="date" class="form-control is-valid" name="datum" value="yyyy-mm-dd" required>
+      <br>
+      <input type="submit" class="btn btn-primary" name="submit" value="Klaar">
+</div>
+<div class="col-md-4 mb-5">
     <label for="commentaar"> Commentaar/Feedback: </label>
-    <textarea id="commentaar" type="text" name="commentaar" rows="8" cols="50"></textarea>
-</p>
-<p class="tableForm">
-    <label for="rating_cv"> Beoordeling CV: </label>
-    <input type="text" name="rating_cv">
-</p>
-<p class="tableForm">
+    <textarea id="commentaar" type="text" class="form-control is-valid" name="commentaar" pattern="[A-Za-z0-9_]{1,15}" rows="8" cols="50"></textarea>
+</div>
+<div class="col-md-1 mb-6">
+    <label for="rating_cv"> Beoordeling CV (1-10): </label>
+    <input type="number" class="form-control is-valid" name="rating_cv" required>
+</div>
+<div class="col-md-1 mb-7">
     <label for="rating_website"> beoordeling Website: </label>
-    <input type="text" name="rating_website">
-</p>
-    <input type="submit" name="submit" value="OK">
+<div class="stars">
+    <input class="star star-5" id="star-5" type="radio" name="star" value="5"/>
+    <label class="star star-5" for="star-5"></label>
+    <input class="star star-4" id="star-4" type="radio" name="star" value="4"/>
+    <label class="star star-4" for="star-4"></label>
+    <input class="star star-3" id="star-3" type="radio" name="star" value="3"/>
+    <label class="star star-3" for="star-3"></label>
+    <input class="star star-2" id="star-2" type="radio" name="star" value="2"/>
+    <label class="star star-2" for="star-2"></label>
+    <input class="star star-1" id="star-1" type="radio" name="star" value="1"/>
+    <label class="star star-1" for="star-1"></label>
+</div>
+    </div>
+</div>
 </form>
 <br>
 <br>
--- review_form_stmt_insert.php
+++ review_form_stmt_insert.php
@@ -9,17 +9,19 @@ if (isset($_POST['submit'])){
         $emailInsert = $_POST['email'];
         $functieInsert = $_POST['functie'];
         $datumInsert = $_POST['datum'];
+        $commentaarInsert = $_POST['commentaar'];
         $ratingCvInsert = $_POST['rating_cv'];
-        $ratingWebsiteInsert = $_POST['rating_website'];
+        $ratingWebsiteInsert = $_POST['star'];
 
         $stmtInsert = $db->prepare("INSERT INTO formulier
         (naam, email, functie, datum, rating_cv, rating_website)
-        VALUES (:naam, :email, :functie, :datum, :rating_cv, :rating_website);"); 
+        VALUES (:naam, :email, :functie, :datum, :commentaar, :rating_cv, :rating_website);"); 
 
         $stmtInsert->bindParam(':naam', $naamInsert, PDO::PARAM_STR);
         $stmtInsert->bindParam(':email', $emailInsert, PDO::PARAM_STR);
         $stmtInsert->bindParam(':functie', $functieInsert, PDO::PARAM_STR);
         $stmtInsert->bindParam(':datum', $datumInsert, PDO::PARAM_STR);
+        $stmtInsert->bindParam(':commentaar', $commentaarInsert, PDO::PARAM_STR);
         $stmtInsert->bindParam(':rating_cv', $ratingCvInsert, PDO::PARAM_STR);
         $stmtInsert->bindParam(':rating_website', $ratingWebsiteInsert, PDO::PARAM_STR);
 
