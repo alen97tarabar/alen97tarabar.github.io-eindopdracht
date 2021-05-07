3f6a3f4ff5f3c6f9f119031b50c8db6077df42f0
alen97tarabar <69684780+alen97tarabar@users.noreply.github.com>
2021-05-07 18:44:44 +0200

    Update review.php

--- review.php
+++ review.php
@@ -26,16 +26,39 @@ $stmt->execute();
 
 ?>
 
-<h1> Beoordelingen </h1>
-<br>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #474747!important; position: static; height: 8.2%; width: 100%;">
+  <a class="navbar-brand" href="#">HOME.</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="nav navbar-nav navbar-right">
+      <li class="nav-item active">
+        <a class="nav-link" href="certificaten.php">CERTIFICATEN. <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="review.php">REVIEWS.</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          TIPS.
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: grey;">
+          <a class="dropdown-item" href="frontend.php">.FRONTEND</a>
+          <a class="dropdown-item" href="backend.php">.BACKEND</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
 <br>
 <h2> Lijst </h2>
 <a href="#cijfer">Gemiddelde cijfer </a>
-<div class="container" style="height: 100% !important;">
+<div class="container">
 
-<div class="item" style="width: 60%; height: 100% !important;">
+<div class="item">
 <h2>Reviews</h2>
-<table class="table table-sm" style="width: 100%; height: 100% !important;">
+<table class="table table-sm">
     <tr>
         <th> Naam </th>
         <th> Email </th>
@@ -75,5 +98,12 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
 </div>
 </div>
+<br>
+<br>
+<center>
+<div class="footer" style="position: fixed; left: 0; bottom: 0; width: 100%; height: 5%; background-color: #279e94; color: white; text-align: center;">
+<p>LinkedIn <a href="https://www.linkedin.com/in/alen-tarabar-543a55159/" style="font-size: 2.2rem !important;" class="fab fa-linkedin"></a> Gmail <a href="mailto:alen97tarabar@gmail.com" style="font-size: 2.2rem !important;" class="fas fa-envelope"></a></p>
+</div>
+</center>
 </body>
 </html>
